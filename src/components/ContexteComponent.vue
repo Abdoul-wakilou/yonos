1a5310c6fcfde50be8169db846bfbc5802591b4a
AbDev - Freelance <87452742+Abdoul-wakilou@users.noreply.github.com>
2024-07-30 13:42:57 +0100

    contexte

--- src/components/ContexteComponent.vue
+++ src/components/ContexteComponent.vue
@@ -1,17 +1,32 @@
 <template>
-    <NavbarComponent></NavbarComponent>
-    <div class="row mt-5">
-      <div class="col-lg-4">
-        <div class="text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci asperiores temporibus totam facilis eius deserunt sequi, maiores quam sunt molestias earum odit provident fugiat tempora distinctio quaerat quos? Accusamus, veritatis.
+    <div>
+    <NavbarComponent />
+    <div class="container section-assurance">
+      <div class="row align-items-center">
+        <div class="col-md-6 text-container order-md-1">
+          <h2 class="title">« L'assurance, c'est compliqué »</h2>
+          <div class="content">
+            <p>Après 15 années d'expérience dans l'assurance, je continue d'entendre cette phrase et je dois avouer que je fais le même constat que vous.</p>
+            <p>En effet, comment choisir son assurance parmi près de 700 assureurs et plus de 40.000 distributeurs ? Pourquoi, il y'a toujours un proche qui paye moins cher que nous ? Pourquoi lors du sinistre les conditions sont difficiles à comprendre ? Comment savoir si je ne possède pas des doublons de garanties? Pourquoi la technologie n'est pas adaptée à notre quotidien ?</p>
+            <p>Afin de trouver la solution qui réponde à toutes ces questions Je me suis nourris des échanges avec mes proches, mes clients et mes pairs. J'ai également effectué différentes recherches et surtout durant mon parcours professionnel, je me suis attelé à exercer dans différents secteurs de l'assurance.</p>
+            <p>Après ces analyses, j'ai donc décidé de créer Mr Yonos, le coach des assurés.</p>
+            <p><strong>Mon ambition est de devenir la référence pour gérer toutes vos assurances au même endroit en ayant des astuces à porter de main.</strong></p>
+            <p class="text-center"><strong>Le monde change et pourquoi pas l'assurance !</strong></p>
+            <p class="signature">Pascal NOLLA</p>
+          </div>
+        </div>
+        <div class="col-md-6 image-container order-md-2">
+          <img src="../assets/img/profile.png" alt="Pascal Nolla">
         </div>
       </div>
-      <div class="col-lg-4">
-        <div class="image">
-          <img src="../assets/img/profil.png" class="img-fluid" alt="coach yonos">
+      <div class="row justify-content-center">
+        <div class="col-12 text-center">
+          <p>Pascal NOLLA - FOUNDER & CEO</p>
+          <p>LinkedIn - Instagram - Twitter - TikTok</p>
         </div>
       </div>
     </div>
+  </div>
   </template>
   
   <script>
@@ -19,14 +34,44 @@ import NavbarComponent from './NavbarComponent.vue';
 
   export default {
     name: 'ContexteComponent',
-    componets: {
+    components: {
       NavbarComponent,
     }
   };
   </script>
   
 <style scoped>
-.text{
+.section-assurance {
+  padding: 20px;
+}
+.section-assurance .image-container {
   text-align: center;
+}
+.section-assurance img {
+  max-width: 100%;
+  height: auto;
+}
+.section-assurance .text-container {
+  text-align: left;
+}
+.section-assurance .title {
+  font-style: italic;
+  font-weight: bold;
+}
+.section-assurance .content p {
+  font-style: italic;
+}
+.section-assurance .signature {
+  text-align: center;
+  font-size: 12px;
+  font-weight: bold;
+}
+@media (max-width: 768px) {
+  .section-assurance .text-container {
+    order: 2;
+  }
+  .section-assurance .image-container {
+    order: 1;
+  }
 }
 </style>
--- src/components/ObjectifComponent.vue
+++ src/components/ObjectifComponent.vue
@@ -6,40 +6,40 @@
             <div class="col-lg-3">
                 <div class="card card-custom">
                     <div class="card-body">
-                        <div class="icon">&#128270;</div>
+                        <div class="icon"><img src="../assets/img/loop.png" class="img-fluid img" alt=""></div>
                         <h5 class="card-title">Obtenir un diagnostic de votre situation</h5>
                         <p class="card-text">Vous trouvez en 2 minutes, toutes les assurances adaptées à votre profil grâce à notre assurance score.</p>
-                        <a href="#" class="btn btn-custom">Trouver mes assurances</a>
+                        <router-link to="/assurrance" class="btn btn-custom">Trouver mes assurances</router-link>
                     </div>
                 </div>
             </div>
             <div class="col-lg-3">
                 <div class="card card-custom">
                     <div class="card-body">
-                        <div class="icon">&#9878;</div>
+                        <div class="icon"><img src="../assets/img/tribunal.png" class="img-fluid img" alt=""></div>
                         <h5 class="card-title">Maîtriser mon budget en assurance</h5>
                         <p class="card-text">Vous choisissez un contrat selon votre budget, grâce à nos conseils pour vos devis et également lors de vos renouvellements.</p>
-                        <a href="#" class="btn btn-custom">Effectuer un devis</a>
+                        <router-link to="/devis" class="btn btn-custom">Effectuer un devis</router-link>
                     </div>
                 </div>
             </div>
             <div class="col-lg-3">
                 <div class="card card-custom">
                     <div class="card-body">
-                        <div class="icon">&#128257;</div>
+                        <div class="icon"><img src="../assets/img/cloud.png" class="img-fluid img" alt=""></div>
                         <h5 class="card-title">Piloter mes contrats au même endroit</h5>
                         <p class="card-text">Vous réalisez vos démarches même en changeant d'assureur, grâce à la centralisation de vos contrats sur notre plateforme.</p>
-                        <a href="#" class="btn btn-custom">Confier mes contrats</a>
+                        <router-link to="/contrat" class="btn btn-custom">Confier mes contrats</router-link>
                     </div>
                 </div>
             </div>
             <div class="col-lg-3">
                 <div class="card card-custom">
                     <div class="card-body">
-                        <div class="icon">&#128161;</div>
+                        <div class="icon"><img src="../assets/img/lampe.png" class="img-fluid img" alt=""></div>
                         <h5 class="card-title">Prendre les bonnes décisions</h5>
                         <p class="card-text">Vous obtenez des conseils pratiques pour apprendre à mieux gérer vos assurances, grâce à nos contenus pédagogiques.</p>
-                        <a href="#" class="btn btn-custom">Comprendre l'assurance</a>
+                        <router-link to="/comprendre-assurance" class="btn btn-custom">Comprendre l'assurance</router-link>
                     </div>
                 </div>
             </div>
@@ -62,8 +62,9 @@ import NavbarComponent from './NavbarComponent.vue';
   .section-title {
     color: #a0418e;
     text-align: center;
-    margin-top: 50px;
-    margin-bottom: 50px;
+    margin-top: 40px;
+    margin-bottom: 20px;
+    font-weight: bold;
 }
 .card-custom {
     text-align: center;
@@ -71,6 +72,9 @@ import NavbarComponent from './NavbarComponent.vue';
 }
 .card-custom .card-body {
     padding: 20px;
+}
+.card-custom .card-body .card-text {
+    font-size: 14px;
 }
 .btn-custom {
     background-color: #ffcc66;
@@ -80,6 +84,9 @@ import NavbarComponent from './NavbarComponent.vue';
     border-radius: 25px;
     padding: 10px 20px;
     width: 100%;
+    font-size: 14px;
+    margin-top: 30px;
+    height: 100%;
 }
 .btn-custom:hover {
     background-color: #ffb833;
@@ -89,6 +96,21 @@ import NavbarComponent from './NavbarComponent.vue';
     font-size: 48px;
     color: #000;
     margin-bottom: 20px;
+    width: auto;
+    height: auto;
+}
+.icon .img {
+    width: 330px;
+    height: 110px;
+}
+@media (max-width: 768px) {
+    .icon .img {
+        width: auto;
+        height: auto;
+    }
+    .btn-custom {
+        width: 60%;
+    }
 }
 .card-title {
     color: #a0418e;
