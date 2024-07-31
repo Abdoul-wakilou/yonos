4370b30954e34d6b1c11d9c76a7cd679840ec508
AbDev - Freelance <87452742+Abdoul-wakilou@users.noreply.github.com>
2024-07-31 11:40:59 +0100

    commencement assurrance

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/yonos/'
+    : '/'
+});
